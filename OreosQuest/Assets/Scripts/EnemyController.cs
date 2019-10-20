851cea57471eea3ae0de8d942420ac40e299705b
David Ruiz <druizso@uoc.edu>
2019-10-20 13:11:47 +0200

    millores enemic

--- OreosQuest/Assets/Scripts/EnemyController.cs
+++ OreosQuest/Assets/Scripts/EnemyController.cs
@@ -12,13 +12,13 @@ public class EnemyController : MonoBehaviour
     private float enemySpeed = 0.5f;
     public float enemyRange;
     private Vector3 enemyPos;
-    private bool goRight= true;
-    private bool isWait = false;
     public int waitTime;
+    private int mindState;
 
     // Start is called before the first frame update
     void Start()
     {
+        mindState = 0;
         //guardem la posició incial del enemic
         enemyPos = transform.position;
 
@@ -31,45 +31,42 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
 
-        MoveEnemyRight();// movem l'enemic cap a la dreta
-        EnemyWait();// Deixem l'enemic en repós
-        //Fem que roti l'enemic
-        MoveEnemyLeft();//Movem l'enemic a l'esquerra
+        EnemyBehaviour();
 
     }
 
     // Fem que l'enemic es mogui cap a la dreta
     void MoveEnemyRight()
     {
-        if (transform.position.z < enemyPos.z + (enemyRange / 2) && goRight)
+        if (transform.position.z < enemyPos.z + (enemyRange / 2))
         {
             transform.Translate(0, 0, 1 * enemySpeed * Time.deltaTime);
         }
         else
         {
-            goRight = false;
-            isWait = true;
+            mindState = 1;
         }
     }
 
     //Fem que l'enemic es mogui cap a l'esquerra
     void MoveEnemyLeft()
     {
-        if (transform.position.z > enemyPos.z - (enemyRange / 2) && !goRight)
+        enemyAnim.SetBool("Eat_b", false);
+        enemyAnim.SetFloat("Speed_f", 0.2f);
+
+        if (transform.position.z > enemyPos.z - (enemyRange / 2))
         {
             transform.Translate(0, 0, -1 * enemySpeed * Time.deltaTime);
         }
         else
         {
-            goRight = true;
-            isWait = true;
+            mindState = 0;
         }
     }
 
     //Donem un temps de repos a l'enemic
     void EnemyWait()
     {
-        if (isWait==true)
         enemyAnim.SetBool("Eat_b", true);
         enemyAnim.SetFloat("Speed_f", 0);
 
@@ -79,5 +76,49 @@ public class EnemyController : MonoBehaviour
     IEnumerator EnemyWaitTime()
     {
         yield return new WaitForSeconds(waitTime);
+        mindState = 3;
+    }
+
+    void EnemyRotate()
+    {
+        int i = 0;
+        while(i<181)
+        {
+            transform.eulerAngles = new Vector3(0, i, 0);
+            i ++;
+            Debug.Log(i);
+        }
+       /* for (int i = 0; i < 180; i++)
+        {
+            transform.eulerAngles = new Vector3(0, 1 * Time.deltaTime, 0);
+            Debug.Log(transform.rotation);
+        }*/
+
+        mindState = 2;
+    }
+
+    void EnemyBehaviour()
+    {
+        switch (mindState)
+        {
+            case 0:
+                MoveEnemyRight();
+                break;
+
+            case 1:
+                EnemyWait();
+                break;
+
+            case 2:
+                MoveEnemyLeft();
+                break;
+
+            case 3:
+                EnemyRotate();
+                break;
+
+            default:
+                break;
+        }
     }
 }
